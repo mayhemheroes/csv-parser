6fb8a8e99a257de62d6c88f7add3fc9eb38759a5
Raj Shah <rnshah9@asu.edu>
2022-12-22 05:10:34 +0000

    Delete FDP

--- mayhem/fuzz_parse_no_header.cpp
+++ mayhem/fuzz_parse_no_header.cpp
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <climits>
 
-#include <FuzzedDataProvider.h>
+#include <fuzzer/FuzzedDataProvider.h>
 #include "csv.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     FuzzedDataProvider provider(data, size);
-    std::string str = provider.ConsumeRandomLengthString();
+    std::string str = provider.ConsumeRandomLengthString(1000);
     std::string_view sv{str};
     csv::parse_no_header(sv);
 
